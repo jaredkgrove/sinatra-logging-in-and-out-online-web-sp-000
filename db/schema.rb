13f961f0ef5df72c109053487a2c33a7987c32a7
Jared Grove <jaredkgrove@gmail.com>
2019-07-12 17:05:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190712165728) do
+ActiveRecord::Schema.define(version: 20190712170245) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.float "balance"
     t.string "password"
   end
 
