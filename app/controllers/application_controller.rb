d4f67f5c933f99a3f13ad2e245289161d4e343b2
Jared Grove <jaredkgrove@gmail.com>
2019-07-12 17:28:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect to '/account'
     end
-    redirect '/'
+    redirect to '/'
   end
 
   get '/account' do
